4ea2cba57dff2d4a18a10d8970de7c019b36c82e
Maria Buzakina <buzakina@gmail.com>
2020-03-19 22:03:40 +0300

    disciplines js, input border, fixed nav removed, fluid container removed

--- app/static/js/buttons.js
+++ app/static/js/buttons.js
@@ -61,42 +61,44 @@ function clearLibrary() {
 }
 
 $(document).ready(function(){
-    function disciplineCheckboxState() {
+    function disciplineState() {
         $('.js-discipline-container').each(function() {
             let container = $(this);
             let inputBase = container.find('.js-discipline-base');
             let inputSuperior = container.find('.js-discipline-superior');
             if(inputSuperior.is(':checked')) {
                 container.removeClass('state0').removeClass('state1').addClass('state2');
-                inputSuperior.one('change', function(e){
-                    e.preventDefault();
-                    inputBase.removeAttr('checked');
-                    inputSuperior.removeAttr('checked');
-                });
             }else if(inputBase.is(':checked')) {
                 container.removeClass('state0').removeClass('state2').addClass('state1');
-                inputBase.one('change', function(e){
-                    e.preventDefault();
-                    inputSuperior.trigger('click');
-                    inputBase.removeAttr('checked','checked');
-                    inputSuperior.attr('checked','checked');
-                });
             }else {
                 container.removeClass('state1').removeClass('state2').addClass('state0');
-                inputBase.one('change', function(e){
-                    e.preventDefault();
-                    inputBase.trigger('click');
-                    inputBase.attr('checked','checked');
-                });
             }
-            console.log(inputBase.is('checked') ? 'base: true' : 'base: false');
-            console.log(inputSuperior.is('checked') ? 'sup: true' : 'sup: true');
         });
     };
+    function disciplineChecker(buttton) {
+        let container = $(buttton).parent('.js-discipline-container');
+        let inputBase = container.find('.js-discipline-base');
+        let inputSuperior = container.find('.js-discipline-superior');
 
-    disciplineCheckboxState();
+        if( container.hasClass('state0')) {
+            inputBase.attr('checked', 'checked');
+            inputSuperior.removeAttr('checked');
+            container.removeClass('state0').removeClass('state2').addClass('state1');
+        } else if ( container.hasClass('state1')) {
+            inputBase.removeAttr('checked');
+            inputSuperior.attr('checked', 'checked');
+            container.removeClass('state0').removeClass('state1').addClass('state2');
+        } else if ( container.hasClass('state2')) {
+            inputBase.removeAttr('checked');
+            inputSuperior.removeAttr('checked');
+            container.removeClass('state1').removeClass('state2').addClass('state0');
+        }
+    }
+
+    disciplineState();
+
     $('.js-discipline-container').css("display","block");
-    $(".js-discipline-container input").on("change", function() {
-        disciplineCheckboxState();
+    $(".js-discipline-button-holder").on("click", function(e) {
+        disciplineChecker(this);
     });
 });
